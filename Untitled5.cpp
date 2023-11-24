51268c2532adb66e3087b0e5cd301de8452e35ed
Rehan Ali maan <130174371+RehanAliMaan@users.noreply.github.com>
2023-11-24 20:57:39 +0500

    Add files via upload

---
+++ Untitled5.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	cout<<"BBB"<<"  "<<"EEEEE"<<"  "<<"FFFFF"<<endl;
+	cout <<"B"<<" "<<"B"<<"  "<<"E"<<"      "<<"F"<<endl;
+	cout <<"B"<<" "<<"B"<<"  "<<"E"<<"      "<<"F"<<endl;
+    cout<<"BBB"<<"  "<<"EEE"<<"    "<<"FFF"<<endl;
+	cout <<"B"<<" "<<"B"<<"  "<<"E"<<"      "<<"F"<<endl;
+	cout <<"B"<<" "<<"B"<<"  "<<"E"<<"      "<<"F"<<endl;
+	cout<<"BBB"<<"  "<<"EEEEE"<<"  "<<"F"<<endl;
+}
